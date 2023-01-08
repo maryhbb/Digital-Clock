3372e1f6835fed7e661528d6dc8a93a01a2b8004
Mary <mary_hbb@yahoo.com>
2023-01-08 01:48:10 +0100

    installing webpack

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+import { myFunc } from "./test";
+
+const hourEl = document.getElementById("hour");
+const minutesEl = document.getElementById("minutes");
+const secondEl = document.getElementById("second");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if (h > 12) {
+        h = h - 12
+        ampm = "PM"
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minutesEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(() => {
+        updateClock()
+    }, 1000)
+}
+
+updateClock();
+
+myFunc();
